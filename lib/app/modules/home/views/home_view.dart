a97b8fafff121d8793242f21a1286737546bb130
alimasudd-mac <alimasudd.dev@gmail.com>
2024-04-19 09:03:55 +0700

    create and slicing home_view.dart

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 import 'package:moneyplan/app/core/colors.dart';
+import 'package:moneyplan/app/routes/app_pages.dart';
 
 import '../controllers/home_controller.dart';
 
@@ -15,35 +16,26 @@ class HomeView extends GetView<HomeController> {
         child: const Icon(Icons.add, color: Colors.white,),
         onPressed: () async {
           // Get.toNamed(Routes.ADD_DATA);
+          controller.textEncrypt();
         },
       ),
       appBar: AppBar(
         backgroundColor: Colors.blueGrey,
         foregroundColor: Colors.white,
-        title: TextField(
-          controller: controller.textController,
-        ),
-        actions: [
-          IconButton(
-              onPressed: (){
-                controller.createUser(
-                    name: controller.textController.text);
-                },
-              icon: Icon(Icons.add))
-        ],
+        title: const Text('Home')
       ),
         body: Padding(
           padding: const EdgeInsets.all(8.0),
           child: StreamBuilder(
-            stream: controller.readUsers(),
+            stream: controller.readRecords(),
             builder: (context, snapshot){
               if(snapshot.hasError){
                 return Text('${snapshot.error}');
               }else if(snapshot.hasData){
-                final users = snapshot.data!;
+                final records = snapshot.data!;
 
                 return ListView(
-                  children: users.map(controller.buildUser).toList(),
+                  children: records.map(controller.buildRecord).toList(),
                 );
               }else{
                 return const Center(child: CircularProgressIndicator());
